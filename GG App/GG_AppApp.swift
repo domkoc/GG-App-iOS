f39c87a64b2e9d85ab4b7ba2985ed795a5571ecb
Kocka Dominik <kocka.dominik@autsoft.hu>
2022-10-23 13:19:39 +0200

    Initial Commit

---
+++ GG App/GG_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GG_AppApp.swift
+//  GG App
+//
+//  Created by Kocka Dominik Csaba on 2022. 10. 23..
+//
+
+import SwiftUI
+
+@main
+struct GG_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GG AppUITests/GG_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GG_AppUITestsLaunchTests.swift
+//  GG AppUITests
+//
+//  Created by Kocka Dominik Csaba on 2022. 10. 23..
+//
+
+import XCTest
+
+final class GG_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
