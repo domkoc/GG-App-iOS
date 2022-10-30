a3eb21f43365820144322acde7e8685dce8234cb
Dominik Kocka <Dominik.Kocka@fodmobilitygroup.com>
2022-10-30 14:11:02 +0100

    Fix generate script

--- swift5_generate.sh
+++ swift5_generate.sh
@@ -1,3 +1,2 @@
 rm -r ../generated/SwaggerClient
 swagger-codegen generate -i https://raw.githubusercontent.com/domkoc/GGApp-OpenAPI/master/swagger.yaml -l swift5 -o ../generated
-sed '/^Pod::Spec.new.*/i before=me\nbefore2=me2' ../generated/SwaggerClient.podspec
